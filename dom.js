8cff89bb3055adc8de16161d36c74c4756c20e05
abhisek-1991 <abhisek.may1991@gmail.com>
2023-07-02 20:37:42 +0530

    changes made

---
+++ dom.js
@@ -0,0 +1,8 @@
+// Get the title element by id and add a black border
+var titleElement = document.getElementById("header-title");
+titleElement.style.border = "1px solid black";
+
+// Get the "Add Items" heading element by class and modify its styling
+var addItemsHeading = document.getElementById("qwe");
+addItemsHeading.style.color = "green";
+addItemsHeading.style.fontWeight = "bold";
