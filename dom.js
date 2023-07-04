c875decc081a62324d25aa9fb2b2a9e60e808d3a
abhisek-1991 <abhisek.may1991@gmail.com>
2023-07-04 21:20:40 +0530

    task 5

--- dom.js
+++ dom.js
@@ -1,6 +1,3 @@
-var items=document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
+var li=document.getElementsByTagName('li');
+li[4].style.backgroundColor='blue';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
