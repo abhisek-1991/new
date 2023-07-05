59187fe6ef80ff0bdc95687bc9af58e4cca80d48
abhisek-1991 <abhisek.may1991@gmail.com>
2023-07-05 13:39:18 +0530

    task 8

--- dom.js
+++ dom.js
@@ -6,3 +6,19 @@ headerTitle.textContent = "Hello " + headerTitle.textContent;
 const item1 = document.querySelector("#items .list-group-item:first-child");
 item1.textContent = "Hello " + item1.textContent;
 
+// Adding delete and edit buttons to each list item
+const listItems = document.querySelectorAll("#items .list-group-item");
+listItems.forEach(item => {
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "btn btn-danger btn-sm float-right mr-2";
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", function() {
+    item.remove();
+  });
+  item.appendChild(deleteButton);
+
+  const editButton = document.createElement("button");
+  editButton.className = "btn btn-primary btn-sm float-right";
+  editButton.textContent = "Edit";
+  item.appendChild(editButton);
+});
