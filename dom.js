ff73c0ad79e41c4ab5c71940e05794b0f169faf4
abhisek-1991 <abhisek.may1991@gmail.com>
2023-07-05 12:24:35 +0530

    task 6

--- dom.js
+++ dom.js
@@ -1,3 +1,18 @@
-var li=document.getElementsByTagName('li');
-li[4].style.backgroundColor='blue';
+//var header= document.querySelector('#main-header');
+//header.style.borderBottom= 'solid 4px #ccc';
 
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+var thirdItem=document.querySelector('#items li:nth-child(3)');
+thirdItem.style.display='none';
+
+var two= document.querySelectorAll(".list-group-item");
+console.log(two);
+two[1].style.backgroundColor='green';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
