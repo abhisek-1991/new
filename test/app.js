01decb505335604409fe175794e014581e210b60
abhisek-1991 <abhisek.may1991@gmail.com>
2023-08-11 19:47:57 +0530

    node js task6

--- test/app.js
+++ test/app.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const http= require('http');
+
+
+
+const server= http.createServer((req,res)=>{
+
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+    
+    
+    //process.exit();
+
+});
+
+server.listen(4000, () => {
+    console.log('Server is listening on port 3000');
+})
