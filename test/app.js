a803c72a2fda459a661befde54c1b73b64e9f4fb
abhisek-1991 <abhisek.may1991@gmail.com>
2023-08-20 15:32:27 +0530

    express task 2

--- test/app.js
+++ test/app.js
@@ -1,19 +1,32 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" placeholder="Product Title"><br>
+      <input type="text" name="size" placeholder="Product Size"><br>
+      <button type="submit">Add Product</button>
+    </form>
+  `);
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+app.post('/product', (req, res, next) => {
+  const productTitle = req.body.title;
+  const productSize = req.body.size;
+  console.log('Product Title:', productTitle);
+  console.log('Product Size:', productSize);
+  res.redirect('/');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-server.listen(5000);
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
